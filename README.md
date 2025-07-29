dc18317f708c36f2af8ac69f00d96e84e184796e
Vincent Battaglia <vincent.battaglia@gmail.com>
2025-07-29 16:40:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mezzaterra
+
+A Svelte web application with a distinctive visual identity.
+
+## Development
+
+```sh
+npm install
+npm run dev
+```
+
+## Building
+
+```sh
+npm run build
+npm run preview
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<div class="logo">
+  <table class="letter-grid">
+    <tbody>
+      <tr>
+        <td class="letter red">M</td>
+        <td class="letter alt">Z</td>
+      </tr>
+      <tr>
+        <td class="letter alt">T</td>
+        <td class="letter red">R</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<style>
+  .logo {
+    font-family: "Zilla Slab", serif;
+    font-weight: 600;
+    font-size: 5rem;
+    color: #2d1810;
+    line-height: 0.9;
+  }
+
+  .letter-grid {
+    border-collapse: collapse;
+  }
+
+  .letter {
+    width: 5.5rem;
+    height: 5.5rem;
+    text-align: center;
+    vertical-align: middle;
+    border: 8px solid #2d1810;
+    background-color: #f5f1e8;
+  }
+
+  .letter.alt {
+    background-color: #e8dcc0;
+  }
+
+  .letter.red {
+    background-color: #e8b4b4;
+  }
+</style>
